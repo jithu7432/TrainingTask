a175589bc947dd8cfc37eee3ff2bb5f4824740d8
Jithin Johnson <jithin.johnson@soti.net>
2022-10-17 17:29:19 +0530

    Added: Battery Daemon

---
+++ BatteryBackground/Program.cs
@@ -0,0 +1,8 @@
+using BatteryAnalytics;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => services.AddHostedService<Worker>())
+    .Build();
+
+await host.RunAsync();
+
