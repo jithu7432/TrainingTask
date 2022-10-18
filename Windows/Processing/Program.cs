d1feae5d0b9caeb2f069ffa4ee435bea4bb09986
Jithin Johnson <jithin.johnson@soti.net>
2022-10-18 23:12:25 +0530

    added c#

---
+++ Windows/Processing/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Processing
+{
+    public class Program
+    {
+        public static void Main() {
+
+            var b = new BatteryUtils();
+            var df = b.GetData();
+
+            var res = b.ComputeDecrement(df);
+            foreach(var x in res) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine();
+            var res2 = BatteryUtils.CalculateChargingBounds(df);
+            var res3 = BatteryUtils.ClassifyCounts(df, res2);
+
+            foreach(var x in res3) {
+                Console.WriteLine(x);
+            }
+        }
+
+    }
+
+
+}
