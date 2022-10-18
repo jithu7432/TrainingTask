fd3eef3f27c3944a0b5bea8e5c3c55788c7bca28
Jithin Johnson <jithin.johnson@soti.net>
2022-10-18 18:20:23 +0530

    added all funcs in python

--- notebook.py
+++ notebook.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 
 import utils
+import sqlalchemy
 import pandas as pd
 
-from utils import calculate_overcharged as calc
-from utils import compute_counts as comp 
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
 
-_df = pd.read_csv('test.csv')
-_df.sort_values(by='TimeStamp', ascending=True, inplace=True) #type: ignore
-_df.drop_duplicates(subset=['TimeStamp'], inplace=True) #type: ignore
-_df.reset_index(drop=True, inplace=True) #type: ignore
+def main():
+    connection_string = "DRIVER={SQL Server Native Client 11.0};SERVER=(localdb)\mssqllocaldb;DATABASE=Battery;TrustedConnection=yes"
+    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+    engine = create_engine(connection_url)
 
-df = _df.copy() #type: ignore
-utils.preprocess(df)
+    # 1: 
+    SQL = "SELECT * FROM Daemon ORDER BY [TimeStamp]"
+    df = pd.read_sql(SQL, engine)
+    df = utils.preprocess(df)
+    bounds = utils.calculate_charging_bounds(df) 
+    cout = utils.classify_counts(df, bounds)
+    print(cout)
 
-dat = calc(df)
+    #2:
+    SQL = "SELECT TOP (3600) * FROM Daemon ORDER BY [TimeStamp] DESC"
+    df = pd.read_sql(SQL, engine)
+    df = utils.preprocess_minute(df)
+    cout = utils.compute_decrement(df)
+    print(cout)
 
-temp = comp(df, dat) #type: ignore
-print(temp)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -2,9 +2,44 @@ import pandas as pd
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df['ChargingStatus'] -= 1
+    df.drop_duplicates(subset=['TimeStamp'], inplace=True) 
+    df.reset_index(drop=True, inplace=True)
     return df
 
-def calculate_overcharged(df: pd.DataFrame) -> list[dict[str, int]]:
+def preprocess_minute(stock: pd.DataFrame) -> pd.DataFrame:
+    df = stock[::-1] # SQL sorts and returns in desc order.
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+def compute_decrement(df: pd.DataFrame):
+    level = df.CurrentLevel.values
+    timestamp = df.TimeStamp.values
+    ns = len(level)
+
+    if ns < 60: raise Exception("Not enough values to process!")
+
+    cout = [(level[0], timestamp[0])]
+    i = 1
+    while i < ns:
+        if level[i-1] > level[i]:
+            cout.append((level[i], timestamp[i]))
+        elif level[i-1] < level[i]:
+            cout.append((level[i], timestamp[i]))
+        i += 1
+
+    nt = len(cout)
+    drop = time = 0 
+    j = 1
+    while j < nt:
+        curr = cout[j]
+        prev = cout[j - 1]
+        if prev[0] > curr[0]:
+            drop += (prev[0] - curr[0])
+            time += (curr[1] - prev[1])
+        j += 1
+    return drop, time 
+
+def calculate_charging_bounds(df: pd.DataFrame) -> list[dict[str, int]]:
     plugged = df.ChargingStatus.values
     timestamp = df.TimeStamp.values
 
@@ -20,7 +55,7 @@ def calculate_overcharged(df: pd.DataFrame) -> list[dict[str, int]]:
             l = timestamp[i]
         if toggled and plugged[i] == 1:
             count += 1
-        elif toggled and (plugged[i] == 0 or (i + 1) == ns):
+        elif toggled and (plugged[i] != 1 or (i + 1) == ns):
             r = timestamp[i-1]
             if count > 0:
                 dat = dict(start=l, end=r)
@@ -31,18 +66,14 @@ def calculate_overcharged(df: pd.DataFrame) -> list[dict[str, int]]:
         i += 1
     return cout
 
-def compute_counts(df: pd.DataFrame, arr: dict[str, int]) -> dict: #type: ignore
+def classify_counts(df: pd.DataFrame, arr: dict[str, int]) -> dict: #type: ignore
     cout = { "BadCount" : 0, "OptimalCount" : 0, "SpotCount" : 0, }
     for x in arr:
         temp = df[df.TimeStamp.between(x['start'], x['end'])] #type: ignore
         ns = len(temp.index)
         if temp.CurrentLevel.max() < 100:
             cout["SpotCount"] += 1
-            continue
-        if ns <= 30:
-            cout["OptimalCount"] += 1
-            continue
-        if ns > 30:
+        else:
             levels = temp.CurrentLevel.values
             g = c = i = 0
             while i < ns:
@@ -54,14 +85,8 @@ def compute_counts(df: pd.DataFrame, arr: dict[str, int]) -> dict: #type: ignore
                 i += 1
                 if i + 1 == ns:
                     g = max(g, c)
-            if g > 30:
+            if g > 1800:
                 cout["BadCount"] += 1
-        return cout
-
-
-
-
-
-
-
-
+            else:
+                cout["OptimalCount"] += 1
+    return cout
