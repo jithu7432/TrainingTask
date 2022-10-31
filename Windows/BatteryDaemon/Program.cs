53568ac57460f536779e60850230502f6cf021a5
Jithin Johnson <jithin.johnson@soti.net>
2022-10-31 11:34:34 +0530

    separate branch

--- Windows/BatteryDaemon/Program.cs
+++
@@ -1,8 +0,0 @@
-using BatteryAnalytics;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => services.AddHostedService<Worker>())
-    .Build();
-
-await host.RunAsync();
-
